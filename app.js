59e892547ece92b519dc05409bf38784864eb893
Yoav Levin <yoav.levin@gmail.com>
2020-05-08 15:02:42 +0300

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String,
+    date: String, 
+  }
+
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  date: () =>{
+      let date = new Date();
+      return 'Todays date ' + date.getFullYear.toString(); 
+  }
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(process.env.PORT ||4000);
+console.log('Listening on port' + app.PORT);
